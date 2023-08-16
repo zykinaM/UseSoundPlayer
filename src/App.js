1f1acac96e54575f14a5da32af0ae97927295ea9
Marina <zykinamar@gmail.com>
2023-08-16 20:46:12 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Player from "./component/player";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Player />
+    </div>
+  );
+}
---
+++ src/component/player.js
@@ -0,0 +1,123 @@
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import romantic from "../assets/romantic.mp3";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
+import { IconContext } from "react-icons";
+
+export default function Player() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState({
+    min: "",
+    sec: ""
+  });
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: "",
+  }); // текущее положение звука в минутах и секундах
+
+  const [seconds, setSeconds] = useState(); // текущая позиция звука в секундах
+
+  const [play, { pause, duration, sound }] = useSound(romantic);
+
+  useEffect(() => {
+    if (duration) {
+      const sec = duration / 1000;
+      const min = Math.floor(sec / 60);
+      const secRemain = Math.floor(sec % 60);
+      setTime({
+        min: min,
+        sec: secRemain
+      });
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([])); // устанавливаем состояние с текущим значением в секундах
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min,
+          sec
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
+  const playingButton = () => {
+    if (isPlaying) {
+      pause(); // приостанавливаем воспроизведение звука
+      setIsPlaying(false);
+    } else {
+      play(); // воспроизводим аудиозапись
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+<div className="component">
+      <h2>Playing Now</h2>
+      <img
+        className="musicCover"
+        src="https://placekitten.com/200/200"
+        alt="Cat"
+      />
+      <div>
+        <h3 className="title">Rubaiyyan</h3>
+        <p className="subTitle">Qala</p>
+      </div>
+      <div>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
+      </div>
+
+
+
+      <div>
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipPrevious />
+          </IconContext.Provider>
+        </button>
+        {!isPlaying ? (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPlayCircle />
+            </IconContext.Provider>
+          </button>
+        ) : (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPauseCircle />
+            </IconContext.Provider>
+          </button>
+        )}
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipNext />
+          </IconContext.Provider>
+        </button>
+      </div>
+    </div>
+  );
+}
